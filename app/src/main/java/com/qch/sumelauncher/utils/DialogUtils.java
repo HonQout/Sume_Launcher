82c87627c2f713dda0783900a2c22b71a164082b
HonQout <98481271+HonQout@users.noreply.github.com>
2025-12-20 13:12:09 +0800

    2025-12-20 Version: 1.1.2(1.2.0beta2)(3) Changes and improvements: 1. Migrated the implementation of SettingsActivity from SharedPreferences to RxPreferenceDataStore. 2. Changed the logic of asking for permission to access fine location. On devices running Android 11 or later, Wi-Fi signal level will be obtained without asking this permission. On devices running Android 10 or earlier, an Wi-Fi icon of 0 signal level will be displayed if this permission is not granted. 3. Added option to control animation visibility.
    Fixed:
    1. The issue of failing to get Wi-Fi signal level on devices running Android 11 or later without granting permission to access fine location.
    2. The issue of requesting permission to access fine location on devices running Android 11 or later.
    3. The issue of hard-coding packageName into code.

--- app/src/main/java/com/qch/sumelauncher/utils/DialogUtils.java
+++ app/src/main/java/com/qch/sumelauncher/utils/DialogUtils.java
@@ -6,17 +6,9 @@ import androidx.appcompat.app.AlertDialog;
 
 public class DialogUtils {
     public static void show(AlertDialog.Builder builder, boolean animation) {
-        if (animation) {
-            builder.show();
-        } else {
-            removeAnimAndShow(builder);
-        }
-    }
-
-    public static void removeAnimAndShow(AlertDialog.Builder builder) {
         AlertDialog dialog = builder.create();
         Window dialogWindow = dialog.getWindow();
-        if (dialogWindow != null) {
+        if (dialogWindow != null && !animation) {
             dialogWindow.getAttributes().windowAnimations = 0;
         }
         if (!dialog.isShowing()) {
