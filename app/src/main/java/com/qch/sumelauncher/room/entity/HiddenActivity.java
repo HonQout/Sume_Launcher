327311ebaac5b6382838a1ab2ff3d7ecdcf37788
Hon Qout <shangguicheng@126.com>
2025-12-27 18:56:51 +0800

    2025-12-26 Version: 1.2.2(1.3.0beta2)(4) Changes and improvements: 1. Added icon of airplane mode to top bar. 2. Added ManageFragment, which offers ability to manage installed apps. 3. Updated some libraries.
    Fixed:
    1. The issue of concurrent modifying list and map, which may lead to ConcurrentModificationException.

---
+++ app/src/main/java/com/qch/sumelauncher/room/entity/HiddenActivity.java
@@ -0,0 +1,10 @@
+package com.qch.sumelauncher.room.entity;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class HiddenActivity {
+    @PrimaryKey
+    private String key;
+}
