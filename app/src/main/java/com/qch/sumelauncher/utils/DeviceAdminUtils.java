a831dccc65de744cdf928c3169c629343247224b
HonQout <98481271+honqout@users.noreply.github.com>
2026-08-01 15:07:12 +0800

    2026-08-01 Changes and improvements: 1. Changed the implementation of top bar. Now, top bar is more like the original status bar, whose icon can be freely added or removed, instead of limited in specific places. 2. Changed the implementation of launcher. Now, launcher page is realized by a big ViewGroup with icons of activities on it. This would have the advantage of not re-measuring the spaces between icons when returning from other activities to launcher. Launcher layout is implemented by ViewPager2 and the ViewGroup mentioned above. This would have the advantage of providing the same using experience without dealing with complex lifecycles. 3. Added Room database to record the layout of icons. This is in preparation for freely modifying the places of icons. 4. Added app drawer to view all applications and search for the ones you want. 5. Added a hint before uninstalling this application, reminding users to install other launcher applications before taking this action. 6. Applied a new limitation to the 2 LinearLayouts of the dock bar. Now these two LinearLayouts will not allow multitouch to avoid redundant action. 7. Removed support for Android 5.x and updated the versions of some libraries. 8. Changed the implementation of the transaction between different fragments from FragmentTransaction to BottomSheetBehavior. 9. Changed the implementation of preference page. Added SettingsActivity as the entrance to preference settings. Removed PermissionActivity. 10. Changed the implementation of battery icon on top bar. 11. Added ringer mode icon to top bar. 12. Added settings to control whether to display an icon on top bar. 13. Adjusted the number of columns of application drawer. Now, this figure will be 4 on phones, 5 on tablets whose screen size is between 600dp and 720dp, and 6 on tablets whose screen size is larger than 720dp. 14. Added a button to lock the screen.
    Fixed:
    1. The issue that index of current screen will be set twice when scrolling between screens each time.
    2. The issue that index of current screen will be set to 1 when switching between layouts.
    3. The issue that icons on top bar don't fit dark mode.
    4. The issue that this app may not fit system windows properly.
    5. The issue that preference data store may encounter NullPointerException potentially.

--- app/src/main/java/com/qch/sumelauncher/utils/DeviceAdminUtils.java
+++ app/src/main/java/com/qch/sumelauncher/utils/DeviceAdminUtils.java
@@ -3,10 +3,13 @@ package com.qch.sumelauncher.utils;
 import android.app.admin.DevicePolicyManager;
 import android.content.ComponentName;
 import android.content.Context;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 
 public class DeviceAdminUtils {
+    private static final String TAG = "DeviceAdminUtils";
+
     public static DevicePolicyManager getDevicePolicyManager(@NonNull Context context) {
         Context appContext = context.getApplicationContext();
         return appContext.getSystemService(DevicePolicyManager.class);
@@ -15,5 +18,19 @@ public class DeviceAdminUtils {
     public static boolean isAdminActive(@NonNull Context context, ComponentName componentName) {
         DevicePolicyManager manager = getDevicePolicyManager(context);
         return manager.isAdminActive(componentName);
+    }
+
+    public static boolean removeActiveAdmin(@NonNull Context context, ComponentName componentName) {
+        DevicePolicyManager manager = getDevicePolicyManager(context);
+        if (manager.isAdminActive(componentName)) {
+            try {
+                manager.removeActiveAdmin(componentName);
+                return true;
+            } catch (SecurityException e) {
+                Log.e(TAG, "Failed to remove active admin.", e);
+                return false;
+            }
+        }
+        return false;
     }
 }
